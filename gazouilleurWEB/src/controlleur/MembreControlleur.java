107cf8561fa272d861c0b8079a8d342f02e53610
nicolas.gaye <nicolas.gaye@8242f094-4522-11de-ac4d-a115ae307fd0>
2009-05-21 12:48:14 +0000

    Système d'authentification opérationnel (pour les bases) Manque la gestion des erreurs: champs vides à l'inscription + mauvais login/mdp Petit prob graphique après deconnexion

--- gazouilleurWEB/src/controlleur/MembreControlleur.java
+++ gazouilleurWEB/src/controlleur/MembreControlleur.java
@@ -3,6 +3,8 @@ package controlleur;
 import javax.ejb.EJB;
 import javax.servlet.http.HttpServlet;
 
+import org.richfaces.component.html.HtmlModalPanel;
+
 import ejb.MembreFacade;
 import entity.Membre;
 
@@ -17,17 +19,48 @@ public class MembreControlleur extends HttpServlet{
 	private static MembreFacade membreFacade;
 	
 	private Membre membre = new Membre();
+	private boolean estConnecte;
+	private String password2;
 	
-	public String doCreerMembre() {
+	private boolean closePanelInscription = false;
+	private boolean closePanelConnexion = false;
+	
+	public String creerMembre() {
+		closePanelInscription = false;
 		try {
-			System.out.println(membreFacade);
-			//membre = membreFacade.creerMembre(membre);
+//			if(!verifierPassword()){
+//				throw new Exception("Les mots de passe ne correspondent pas.");
+//			}
+			System.out.println(membre);
+			//System.out.println(membre);
+			membre = membreFacade.creerMembre(membre);
+			closePanelInscription = true;
+			estConnecte = true;
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-//		login = password;
+		//membre.setPseudo(membre.getPassword());
+		//System.out.println(membre);
 		return "membre.cree";
+	}
+	
+	public String deconnexion(){
+		membre = new Membre();
+		estConnecte = false;
+		return "deconnexion";
+	}
+	
+	public String connexion(){
+		closePanelConnexion = false;
+		membre = membreFacade.connexionMembre(membre.getPseudo(), membre.getPassword());
+		estConnecte = true;
+		closePanelConnexion = true;
+		return "connexion";
+	}
+	
+	private boolean verifierPassword() {
+		return password2.equals(membre.getPassword());
 	}
 
 	public Membre getMembre() {
@@ -36,5 +69,37 @@ public class MembreControlleur extends HttpServlet{
 
 	public void setMembre(Membre membre) {
 		this.membre = membre;
+	}
+
+	public String getPassword2() {
+		return password2;
+	}
+
+	public void setPassword2(String password2) {
+		this.password2 = password2;
+	}
+
+	public boolean isClosePanelInscription() {
+		return closePanelInscription;
+	}
+
+	public void setClosePanelInscription(boolean closePanelInscription) {
+		this.closePanelInscription = closePanelInscription;
+	}
+
+	public boolean isEstConnecte() {
+		return estConnecte;
+	}
+
+	public void setEstConnecte(boolean estConnecte) {
+		this.estConnecte = estConnecte;
+	}
+
+	public boolean isClosePanelConnexion() {
+		return closePanelConnexion;
+	}
+
+	public void setClosePanelConnexion(boolean closePanelConnexion) {
+		this.closePanelConnexion = closePanelConnexion;
 	}
 }
