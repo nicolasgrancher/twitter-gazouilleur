7b494e5de92685e03c95b590e0f7b559eb2f7946
bellolive@gmail.com <bellolive@gmail.com@8242f094-4522-11de-ac4d-a115ae307fd0>
2009-05-30 14:17:08 +0000

    première version JMS/MDB non testée (les tests unitaires arrivent / la méthode sendMail est à implémenter)

---
+++ trunk/gazouilleurEJB/ejbModule/MessageDrivenBeans/MsgPublicMDB.java
@@ -0,0 +1,46 @@
+package MessageDrivenBeans;
+
+import javax.ejb.EJBException;
+import javax.ejb.MessageDriven;
+import javax.ejb.ActivationConfigProperty;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.ObjectMessage;
+
+import com.sun.xml.internal.messaging.saaj.packaging.mime.MessagingException;
+
+import entity.MessagePublic;
+
+@MessageDriven(mappedName = "jms/topic/MsgPublicTopic", activationConfig = {
+	@ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Topic"),
+	@ActivationConfigProperty(propertyName = "subscriptionDurability", propertyValue = "Durable"),
+	@ActivationConfigProperty(propertyName = "clientId", propertyValue = "emailSenderID"),
+	@ActivationConfigProperty(propertyName = "subscriptionName", propertyValue = "EmailSender")
+})
+
+public class MsgPublicMDB implements MessageListener {
+	
+	@Override
+	public void onMessage(Message message) {
+        try {
+            if (message instanceof ObjectMessage) {
+                ObjectMessage msg = (ObjectMessage) message;
+                MessagePublic messagePublic = (MessagePublic) msg.getObject();
+                sendEMail(messagePublic);
+            } else {
+                // Créer une exception?
+            }
+        } catch (JMSException e) {
+            throw new EJBException(e);
+        } catch (MessagingException e) {
+            throw new EJBException(e);
+        }
+   }
+	
+	private void sendEMail(MessagePublic messagePublic) throws MessagingException {
+		// Envoyer un message à tous les suiveurs de l'emetteur (present dans le header du message reçus)
+	
+	}
+	
+}
